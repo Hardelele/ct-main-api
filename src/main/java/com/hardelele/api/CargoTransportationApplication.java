e1187ee56bc9b175c7c57529927336091b157f57
Hardelele <i.lapshina@mail.ru>
2019-12-07 07:52:57 +0300

    Add CargoTransportationApplication and mark it as SpringBootApplication

---
+++ src/main/java/com/hardelele/api/CargoTransportationApplication.java
@@ -0,0 +1,12 @@
+package com.hardelele.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoTransportationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargoTransportationApplication.class, args);
+    }
+}
