a66333649c25d5d527add2b19893bba14e544563
Hardelele <i.lapshina@mail.ru>
2019-12-08 17:02:35 +0300

    CargoTransportationApplication: wired spring and vertx in @PostConstruct

--- src/main/java/com/hardelele/api/CargoTransportationApplication.java
+++ src/main/java/com/hardelele/api/CargoTransportationApplication.java
@@ -1,12 +1,25 @@
 package com.hardelele.api;
 
+import io.vertx.core.Vertx;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CargoTransportationApplication {
 
+    @Autowired
+    private ServerVerticle serverVerticle;
+
     public static void main(String[] args) {
         SpringApplication.run(CargoTransportationApplication.class, args);
+    }
+
+    @PostConstruct
+    public void deployVerticle() {
+        final Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(serverVerticle);
     }
 }
