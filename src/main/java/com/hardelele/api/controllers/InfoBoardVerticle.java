55192db56693836128277afba915901eb395e5bf
Hardelele <i.lapshina@mail.ru>
2019-12-09 09:01:46 +0300

    InfoBoardVerticle: wired InfoBoardService and make getAll method

--- src/main/java/com/hardelele/api/controllers/InfoBoardVerticle.java
+++ src/main/java/com/hardelele/api/controllers/InfoBoardVerticle.java
@@ -1,4 +1,22 @@
 package com.hardelele.api.controllers;
 
+import com.hardelele.api.models.InfoBoardModel;
+import com.hardelele.api.services.InfoBoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class InfoBoardVerticle {
+
+    InfoBoardService infoBoardService;
+
+    @Autowired
+    public InfoBoardVerticle(InfoBoardService infoBoardService) {
+        this.infoBoardService = infoBoardService;
+    }
+
+    public List<InfoBoardModel> getAll() {
+        return infoBoardService.getAll();
+    }
+
 }
