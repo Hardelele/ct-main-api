8bd0b6f82991708f476ac24f4960092fa7225ca8
Hardelele <i.lapshina@mail.ru>
2019-12-09 09:02:17 +0300

    CargoTransportationApplication: add logger and write log.info

--- src/main/java/com/hardelele/api/CargoTransportationApplication.java
+++ src/main/java/com/hardelele/api/CargoTransportationApplication.java
@@ -1,6 +1,8 @@
 package com.hardelele.api;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,8 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class CargoTransportationApplication {
+
+    Logger log = LoggerFactory.getLogger(ServerVerticle.class);
 
     @Autowired
     private ServerVerticle serverVerticle;
@@ -20,6 +24,7 @@ public class CargoTransportationApplication {
     @PostConstruct
     public void deployVerticle() {
         final Vertx vertx = Vertx.vertx();
+        log.info("Starting deploy ServerVerticle!");
         vertx.deployVerticle(serverVerticle);
     }
 }
